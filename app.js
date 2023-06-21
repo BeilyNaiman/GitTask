5bc5517c44a6b09b6d6424911f07ab0e3b3d374f
BeilyNaiman <beily06407@gmail.com>
2023-06-21 18:19:52 +0300

    fullProject

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("app is running")
+const router=require('./router/userRouter') 
+const express=require('express')
+const app=express()
+const PORT= process.env.PORT|| 3000;
+app.use(express.json());
+app.use(router);
+app.listen(PORT,()=>console.log("server is running in port "))
